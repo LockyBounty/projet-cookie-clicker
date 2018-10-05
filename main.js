ae67c36a544ad6dc957cc11387202263dd434be3
Locky <inox127@hotmail.com>
2018-10-05 15:07:16 +0200

    added stuff

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 let aff= document.querySelector("#affichage");
 let score=0;
+let scoreOutput=0;
+let affMulti= document.querySelector("#multiplicateur");
+let scoreMulti=1;
+affMulti.innerHTML=`${scoreMulti}`;
+
+
+let plusUn = (event) => {
+    console.log('Incrementation OK');
+    score++;
+    scoreOutput = score * scoreMulti;
+    aff.innerHTML=`${scoreOutput}`;
+    
+}
+
+let augmenterMultiplicateur = (event) => {
+    console.log('multiplication OK');
+    scoreMulti++;
+    affMulti.innerHTML=`${scoreMulti}`;
+}
+
+
+
+
+document.querySelector("#boutonPlus").addEventListener("click", plusUn);
+document.querySelector("#boutonMulti").addEventListener("click", augmenterMultiplicateur);
 
