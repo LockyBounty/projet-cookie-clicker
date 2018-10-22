4b01b2d7752435a87bd0e50ce9cb0d88bb6592a2
Locky <inox127@hotmail.com>
2018-10-22 16:52:28 +0200

    added lines

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-let aff= document.querySelector("#boutonPlus");
-let score=0;
-let scoreOutput=0;
-let multiplier= document.querySelector("#multiply");
-let scoreMulti=1;
-multiplier.value=`${scoreMulti}`;
+let aff = document.querySelector("#boutonPlus");
+let score = 0;
+let scoreOutput = 0;
+let multiplier = document.querySelector("#multiply");
+let scoreMulti = 1;
+multiplier.value = `${scoreMulti}`;
 
 
 
 
 let plusUn = (event) => {
-        
-        scoreOutput = score*this.scoreMulti; 
-    
-        console.log(`${scoreOutput}`);
-    
-    
-    aff.value=`${scoreOutput}`;
-    
+    if (scoreMulti === 1) {
+        scoreOutput = score++;
+        aff.value = `${scoreOutput}`;
+    }
+    else {
+        scoreOutput += score*scoreMulti;
+        aff.value = `${scoreOutput}`;
+    }
 }
 
-let increaseMultiplicateur = (event) => {
+let increaseMultiplier = (event) => {
     console.log('multiplication OK');
     scoreMulti++;
-    multiplier.value=`${scoreMulti}`;
-
+    multiplier.value = `${scoreMulti}`;
+    return scoreMulti;
 }
 
 
 
 
 document.querySelector("#boutonPlus").addEventListener("click", plusUn);
-document.querySelector("#boutonMulti").addEventListener("click", augmenterMultiplicateur);
-
+document.querySelector("#multiply").addEventListener("click", increaseMultiplier);
